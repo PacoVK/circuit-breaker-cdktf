5720ccb1a63411050b663bfc158ff8699a1de7ff
Pascal Euhus <pascal.euhus@new-work.se>
2022-08-19 09:07:54 +0200

    add CI

--- infrastructure/__tests__/vpc.test.ts
+++ infrastructure/__tests__/vpc.test.ts
@@ -10,10 +10,13 @@ describe("Networking code test", () => {
       expect(Testing.fullSynth(stack)).toBeValidTerraform();
     });
 
+    /*
+    TODO: uncomment if you provide valid AWS credentials (eg. in CI context)
     it("check if this can be planned", () => {
       const app = Testing.app();
       const stack = new HarbourVPC(app, "network");
       expect(Testing.fullSynth(stack)).toPlanSuccessfully();
     });
+     */
   });
 });
